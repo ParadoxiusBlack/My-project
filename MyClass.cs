4736403f5b840c6a4e352c825f894fa99f65cb0e
ParadoxiusBlack <98045278+ParadoxiusBlack@users.noreply.github.com>
2022-09-09 16:37:26 -0600

    Added ninja stats

--- MyClass.cs
+++ MyClass.cs
@@ -1,1 +1,19 @@
+using System;
+	
+public class Program
+{
+	public Stats ninja;
+		
+	public void Main()
+    {
+		ninja = new Stats();
+		
+		ninja.stealth = 3.5;
+		Console.WriteLine("Stealth Factor: " + ninja.stealth);
+	}
+}
 
+public class Stats
+{
+		public double stealth;
+}
