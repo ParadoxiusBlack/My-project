b6c51fc527121932492e781e42ff71df6029ad09
ParadoxiusBlack <98045278+ParadoxiusBlack@users.noreply.github.com>
2022-09-14 13:22:53 -0600

    Added conditions

--- Conditionals.cs
+++ Conditionals.cs
@@ -1,1 +1,27 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		int x = 50;
+		int y = 50;
+		if (x == y)
+		{
+			Console.WriteLine("X is Equal to Y");
+		}
+		if (x > y)
+		{
+			Console.WriteLine("X is Greater than Y");
+		}
+		if (x < y)
+		{
+			Console.WriteLine("X is Less Than Y");
+		}
+		if (x != y)
+		{
+			Console.WriteLine("X is not Equal to Y");
+		}
+	}
+}
