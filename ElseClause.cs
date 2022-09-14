f4a7ba73a37913e4bb1a7c799f3759a3b2629502
ParadoxiusBlack <98045278+ParadoxiusBlack@users.noreply.github.com>
2022-09-14 13:24:44 -0600

    Added ElseClause example

--- ElseClause.cs
+++ ElseClause.cs
@@ -1,1 +1,23 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		int x = 60;
+		int y = 70;
+		if (x == y)
+		{
+			Console.WriteLine("X is Equal to Y");
+		}
+		else if (x > y)
+		{
+			Console.WriteLine("X is Greater than Y");
+		}
+		else if (x < y)
+		{
+			Console.WriteLine("X is Less Than Y");
+		}
+	}
+}
