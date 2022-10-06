bf5615b0505761bc1e1b2e606d8d6cc83466e3e9
Connor Preece <cjp3737@gmail.com>
2022-10-05 20:05:34 -0600

    An assortment of more scripts
    There's so many

---
+++ FallClassGame/Assets/AwakeAndStart.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake ()
+    {
+        Debug.Log("Awake has been called.");
+    }
+    
+    
+    void Start ()
+    {
+        Debug.Log("Start has been called.");
+    }
+}
